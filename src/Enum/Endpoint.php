6f21e998dd5522c41ecc90b3bb9c81ae085ca0d6
Camoo Dev <etn@camoo.sarl>
2025-11-04 14:13:48 +0100

    fix typo

--- src/Enum/Endpoint.php
+++ src/Enum/Endpoint.php
@@ -6,7 +6,7 @@ namespace Enkap\OAuth\Enum;
 
 enum Endpoint: string
 {
-    case ENKAP_API_URL_LIVE = 'api-v2.enkap.cm';
+    case ENKAP_API_URL_LIVE = 'https://api-v2.enkap.cm';
 
     case ENKAP_API_URL_SANDBOX = 'https://api.enkap-staging.maviance.info';
 
